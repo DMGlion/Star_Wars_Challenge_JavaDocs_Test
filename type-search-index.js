f2294277e4ab251a4dbb09df8d636f0fa56e36d0
David Moya <davidmgarcia6@gmail.com>
2019-10-04 12:18:09 +0100

    Added javadocs test doc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"SpaceShipsAndCalculation","l":"SpaceShipsOperationsTest"},{"p":"<Unnamed>","l":"StarWarsAPItest"},{"p":"<Unnamed>","l":"UserActionScannerTest"}]
